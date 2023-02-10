c692cad0b7ecc38fe440c2f97f8d37b21776811e
Payman <115703785+PeymanFakoori@users.noreply.github.com>
2023-02-10 11:56:07 +0330

    done

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log(1);
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get("/firstApi", function (req, res) {
+  console.log(1);
+  res.send("Hello from server");
+});
+app.get("/users", function (req, res) {
+  const users = ["Taha", "Sina"];
+  res.send(users);
+});
+app.post("/buffer", function (req, res) {
+  console.log(req.body);
+  let body = req.body;
+  body.date = new Date();
+  body.id = Math.ceil(Math.random() * 1000);
+  res.send(body);
+});
+app.listen(5000);
+// module.method("a", "b", function(param1, param2) {
+// param1....
+// param2....
+// });
+// fs.readFile("address", function(err, data) {
+// });
